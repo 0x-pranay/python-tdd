c57773e2c3bb4b6bfcae56b4a8f7581980ab6051
velisoju pranay <velisojupranay@gmail.com>
2019-03-07 22:32:37 +0530

    First FT specced out in comments, and now uses unittest.

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,29 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+# functional Tests: to see how the application functions fromt he isers point of view
+# Minimum viabel app: Simplest thing we can build that is still useful.
 
-assert 'Django' in browser.title
+
+# FT should have a human readale story that we can follow.
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check its homepage
+        self.browser.get('http://localhost:8000')
+
+        # she notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+
+if __name__== '__main__':
+    unittest.main(warnings='ignore')
