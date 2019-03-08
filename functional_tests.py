33f78d1dae1b16ae1748d2560f1a31c030633b71
velisoju pranay <velisojupranay@gmail.com>
2019-03-08 23:43:36 +0530

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
-# functional Tests: to see how the application functions fromt he isers point of view
+# functional Tests: to see how the application functions fromt he users
+# point of view
 # Minimum viabel app: Simplest thing we can build that is still useful.
 
 
@@ -22,8 +25,31 @@ class NewVisitorTest(unittest.TestCase):
 
         # she notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # she is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+
+        self.assertEqual(inputbox.get_attribute('placeholder'),
+                         'Enter a to-do item'
+                         )
+        # She types buy peacock feathers int o a text box LOL
+        inputbox.send_keys('Buy peacock feathers')
+
+        # when she hits enter the page updates, and now th page lists
+        # "1: Buy peacock feathers" as an item in  a to-do list table
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
         self.fail('Finish the test!')
 
 
-if __name__== '__main__':
+if __name__ == '__main__':
     unittest.main(warnings='ignore')
