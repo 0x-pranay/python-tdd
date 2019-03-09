09c6ca17f6b36c5db20d7eeb70aced61bfe8f563
velisoju pranay <velisojupranay@gmail.com>
2019-03-09 15:39:40 +0530

    Refractor home page view to use a template

--- functional_tests.py
+++ functional_tests.py
@@ -43,7 +43,7 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
+        rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
             any(row.text == '1: Buy peacock feathers' for row in rows)
         )
