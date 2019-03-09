cbfc4286bf41bf51aadeb2696aaf29a2ae172984
velisoju pranay <velisojupranay@gmail.com>
2019-03-09 23:55:00 +0530

    Done FT refactor for viewing 2 lists

--- functional_tests.py
+++ functional_tests.py
@@ -7,8 +7,8 @@ import unittest
 # point of view
 # Minimum viabel app: Simplest thing we can build that is still useful.
 
-
 # FT should have a human readale story that we can follow.
+
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -18,16 +18,19 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check its homepage
         self.browser.get('http://localhost:8000')
-
         # she notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
-
         # she is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
 
@@ -36,11 +39,11 @@ class NewVisitorTest(unittest.TestCase):
                          )
         # She types buy peacock feathers int o a text box LOL
         inputbox.send_keys('Buy peacock feathers')
-
         # when she hits enter the page updates, and now th page lists
         # "1: Buy peacock feathers" as an item in  a to-do list table
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(2)
+        time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is stil a text box inviting her to add another itemself.
         # She enters a second list :"Use peacock feathers to make a fly"
@@ -50,16 +53,10 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # the page updates again and now shows both lists on her list
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        # self.assertTrue(
-        #     any(row.text == '1: Buy peacock feathers' for row in rows),
-        #     f"New to-do item did not appear in table. Contents were: \n {table.text}"
-        # )
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
-
+        # Edith wonders whether the site will remember her list. Then she sees
         self.fail('Finish the test!')
 
 
--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-
 
 def home_page(request):
+
     # if request.method == 'POST':
     #     return HttpResponse(request.POST['item_text'])
     # return render(request, 'home.html')
