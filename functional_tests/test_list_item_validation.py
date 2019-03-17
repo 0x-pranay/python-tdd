b0d334908253c34475a953900a2a03e3e9a0df91
velisoju pranay <velisojupranay@gmail.com>
2019-03-17 20:00:20 +0530

    Added FT for list_item_validation - cannot add emptylist item

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,5 +1,4 @@
 from selenium.webdriver.common.keys import Keys
-from unittest import skip
 from .base import FunctionalTest
 # functional Tests: to see how the application functions fromt he users
 # point of view
@@ -14,16 +13,31 @@ class ItemValidationTest(FunctionalTest):
 
         # Edith goes to homepage and accidentally tries to submit
         # an empty list item. She hits on the empty input box.
+        self.browser.get(self.live_server_url)
+
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # The homepage refreshes and there is an error saying that list
         # items cannot be blank
-
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You can't have an empty list item"
+        ))
         # She tries again with some text in the input box, which now works.
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
 
         # Perversely she now decides to submit blank list items
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # she receives a similar warning on the list page.
-
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You can't have an empty list item"
+        ))
         # And She correct it by filling some text in it.
-
-        self.fail('write me!')
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+        self.wait_for_row_in_list_table('2: Make tea')
