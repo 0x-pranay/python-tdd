f8c422a4c1555c1a2980ebbe9939b4c5ac5f5c1e
velisoju pranay <velisojupranay@gmail.com>
2019-04-14 23:40:30 +0530

    tests and refactoring for Form validation- duplication, ordering and string representation

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -43,3 +43,31 @@ class ListAndItemsModelsTest(TestCase):
         list_ = List.objects.create()
         self.assertEqual(list_.get_absolute_url(),
                          '/lists/{}/'.format(list_.id))
+
+    def test_duplicate_items_are_invalid(self):
+        list_ = List.objects.create()
+        Item.objects.create(list=list_, text='bla')
+        with self.assertRaises(ValidationError):
+            item = Item(list=list_, text='bla')
+            item.full_clean()
+
+    def test_CAN_save_same_item_to_different_lists(self):
+        list1 = List.objects.create()
+        list2 = List.objects.create()
+        Item.objects.create(list=list1, text='bla')
+        item = Item(list=list2, text='bla')
+        item.full_clean()  # should not raise
+
+    def test_list_ordering(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='il')
+        item2 = Item.objects.create(list=list1, text='item2')
+        item3 = Item.objects.create(list=list1, text='item3')
+        self.assertEqual(
+            list(Item.objects.all()),
+            [item1, item2, item3]
+        )
+
+    def test_string_representation(self):
+        item = Item(text='some text')
+        self.assertEqual(str(item), 'some text')
